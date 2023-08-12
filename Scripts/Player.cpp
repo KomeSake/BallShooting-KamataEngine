8b0b3bf1d797fe6aa21d086aad35f2ca95516e17
米酒哇 <komesakewa@gmail.com>
2023-08-12 16:51:11 +0900

    碰撞检测已完成！赶紧备份！

--- Scripts/Player.cpp
+++ Scripts/Player.cpp
@@ -38,33 +38,54 @@ void Player::Move(char keys[], float bgWidth, float bgHeight, float minMapSize)
 		_dir.x = _dir.x / vectorLength;
 		_dir.y = _dir.y / vectorLength;
 	}
-	_pos.x += _dir.x * _speed;
-	_pos.y += _dir.y * _speed;
 
 	//格子限制
-	////注意！地图格子是二维容器装着的，所以序号是从0开始
-	int checkUp = (int)((bgHeight - _pos.y - _height / 2) / minMapSize);
-	int checkDown = (int)((bgHeight - _pos.y + _height / 2) / minMapSize);
-	int checkLeft = (int)((_pos.x - _width / 2) / minMapSize);
-	int checkRight = (int)((_pos.x + _width / 2) / minMapSize);
+	//注意！地图格子是二维容器装着的，所以序号是从0开始
+
+	//先计算出当前所在的格子
 	int playerCheckRow = (int)((bgHeight - _pos.y) / minMapSize);
 	int playerCheckLine = (int)(_pos.x / minMapSize);
 
-	Novice::ScreenPrintf(10, 100, "%d,%d,%d,%d,", checkUp, checkDown, checkLeft, checkRight);
+	//开始x轴的移动，移动后算出新的上下左右4个角的格子位置
+	_pos.x += _dir.x * _speed;
+	int checkUp = (int)((bgHeight - _pos.y - _height / 2) / minMapSize);
+	int checkDown = (int)((bgHeight - _pos.y + _height / 2 - 1) / minMapSize);//下和右格要缩小一像素，这样才准确
+	int checkLeft = (int)((_pos.x - _width / 2) / minMapSize);
+	int checkRight = (int)((_pos.x + _width / 2 - 1) / minMapSize);
+	//通过4个角的格子来判断是否已经不在可通过格子中，如果不在就把player移回去
+	if (_dir.x > 0) {
+		if (Map::_mapData1[checkUp][checkRight] != 'o'
+			|| Map::_mapData1[checkDown][checkRight] != 'o') {
+			_pos.x = playerCheckLine * minMapSize + _width / 2;
+		}
+	}
+	else if (_dir.x < 0) {
+		if (Map::_mapData1[checkUp][checkLeft] != 'o'
+			|| Map::_mapData1[checkDown][checkLeft] != 'o') {
+			_pos.x = playerCheckLine * minMapSize + _width / 2;
+		}
+	}
 
-	if (Map::_mapData1[checkUp][playerCheckLine] != 'o') {
-		_pos.y = bgHeight - playerCheckRow * minMapSize - _height / 2;
+	//y轴也是相同的，先移动后判断，如果不对劲就把player移回去
+	//核心区别就在于，之前都是x轴和y轴都移动了后再进行判断，这样会导致后面的轴被前面影响而导致误判
+	_pos.y += _dir.y * _speed;
+	checkUp = (int)((bgHeight - _pos.y - _height / 2) / minMapSize);
+	checkDown = (int)((bgHeight - _pos.y + _height / 2 - 1) / minMapSize);
+	checkLeft = (int)((_pos.x - _width / 2) / minMapSize);
+	checkRight = (int)((_pos.x + _width / 2 - 1) / minMapSize);
+	if (_dir.y > 0) {
+		if (Map::_mapData1[checkUp][checkLeft] != 'o'
+			|| Map::_mapData1[checkUp][checkRight] != 'o') {
+			_pos.y = bgHeight - playerCheckRow * minMapSize - _height / 2;
+		}
 	}
-	else if (Map::_mapData1[checkDown][playerCheckLine] != 'o') {
-		_pos.y = bgHeight - playerCheckRow * minMapSize - _height / 2;
+	else if (_dir.y < 0) {
+		if (Map::_mapData1[checkDown][checkLeft] != 'o'
+			|| Map::_mapData1[checkDown][checkRight] != 'o') {
+			_pos.y = bgHeight - playerCheckRow * minMapSize - _height / 2;
+		}
 	}
-	if (Map::_mapData1[playerCheckRow][checkLeft] != 'o') {
-		_pos.x = playerCheckLine * minMapSize + _width / 2;
-	}
-	else if (Map::_mapData1[playerCheckRow][checkRight] != 'o') {
-		_pos.x = playerCheckLine * minMapSize + _width / 2;
-	}
-
+	//Novice::ScreenPrintf(10, 100, "u:%d,d:%d,l:%d,r:%d,", checkUp, checkDown, checkLeft, checkRight);
 
 	//最外围的边界限制
 	if (_pos.x + _width / 2 > bgWidth) {
